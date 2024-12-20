8a465933e8af63e275dda4344a2a19fb3e5fbf7c
Lilyan Hatim Hassan <lilyanhatim@gmail.com>
2024-12-21 00:28:59 +0300

    updated
    To do list project is completed :)

--- script.js
+++ script.js
@@ -14,9 +14,11 @@ while (count < 1) {
             document.body.appendChild(h4);
             var checkbox = document.createElement('input');
             checkbox.setAttribute('type', 'checkbox');
+            checkbox.setAttribute('id','checkBox');
             document.body.appendChild(checkbox);
         }
         li = document.createElement('li');
+         document.querySelector("input").value = ''; //to empty the input field after adding the task!
     });
     count++;
 };
