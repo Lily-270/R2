0856aae9b3b6fa7aaf5c9eed810926cc9e3659d4
Lilyan Hatim Hassan <lilyanhatim@gmail.com>
2025-02-08 23:44:30 +0300

    Updated
    Added the functionality!

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,26 +1,24 @@
-import logo from './logo.svg';
 import { Provider } from "./components/ui/provider"
-import { Flex,Image, Heading, Box ,Center, VStack} from '@chakra-ui/react'
+import { Flex, Image, Heading, Box, Center, VStack} from '@chakra-ui/react'
 import './App.css';
 import Task from "./Task"
 function App() {
+
   return (
     <div className="App">
-      <body>
       <Provider>
         <Flex gap="2" direction="column">
-        <Heading size="3xl" >My To do List !</Heading>
-        <Center>
-        <VStack gap="20" >
-        <Box boxSize="17vh">
-          <Image src="https://i.pinimg.com/736x/ee/d1/87/eed187da6c1f93b1933b25cbaa51c45f.jpg" alt="list-image" />
-        </Box>
-        <Task />
-        </VStack>
-        </Center>
+          <Heading size="3xl" >My To do List !</Heading>
+          <Center>
+            <VStack gap="20" >
+              <Box boxSize="17vh">
+                <Image src="https://i.pinimg.com/736x/ee/d1/87/eed187da6c1f93b1933b25cbaa51c45f.jpg" alt="list-image" />
+              </Box>
+              <Task />
+            </VStack>
+          </Center>
         </Flex>
       </Provider>
-      </body>
     </div>
   );
 }
--- to-do-list/src/Task.js
+++ to-do-list/src/Task.js
@@ -1,38 +1,36 @@
 import './App.css';
 import { useState } from "react"
-import { Field, FieldRoot, Input, Button, VStack } from "@chakra-ui/react"
+import { Field, FieldRoot, Input, Button, VStack, List } from "@chakra-ui/react"
 export default function Task() {
-    const [tasks, setTasks] = useState([]);
 
-    const [inputField, setInput] = useState("");
+
+    const [inputField, setInput] = useState('');
     function handleChange(e) {
         setInput(e.target.value);
     }
-
-
-    function addTask() {
-        if (inputField.trim() !== "") {
+    const [tasks, setTasks] = useState([]);
+    function handleClick() {
+        if (inputField !== "") {
             setTasks([...tasks, inputField]);
             setInput("");
+            console.log([...tasks, inputField]);
         }
-        return(
-            <ul>
-            {tasks.map((x, index) => (
-                <li key={index}>{x}</li>
-            ))}
-        </ul>
-        )
-
-    };
-
+    }
     return (
         <div className="Task">
             <VStack>
                 <FieldRoot orientation="vertical">
                     <Field.Label fontSize="lg" >Type a task!</Field.Label>
-                    <Input placeholder="task" flex="2" size="lg" value={inputField} onChange={handleChange} />
+                    <Input placeholder="task" flex="3" size="lg" value={inputField} onChange={handleChange} />
                 </FieldRoot>
-                <Button size="md" onClick={addTask}>Add task!</Button>
+                <Button size="md" onClick={handleClick}>Add task!</Button>
+                <List.Root>
+                    {tasks.map(function (item, index) {
+                        return (
+                            <List.Item key={index} _marker={{ color: "lightPink" }}>{item}</List.Item>
+                        )
+                    })}
+                </List.Root>
             </VStack>
         </div>
     )
